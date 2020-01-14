811a6b1e413150e5c37c87e50f47e83b9421bc02
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-14 02:23:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,23 +1,10 @@
 # Your Code Here
 
 def generalized_map(source_array)
-  index = 0
-  new_array = []
-    while index < source_array.length do
-      negative = source_array[index] * (-1)
-      new_array << negative
-      index += 1
-    end
-  new_array
+  
 end
 
 def generalized_reduce (source_array, starting_value = 0)
-  index = 0
-  new_array = []
-    while index < source_array.length do
-      new_array << source_array[index]
-      index += 1
-    end
-  new_array
+ 
 end
 
