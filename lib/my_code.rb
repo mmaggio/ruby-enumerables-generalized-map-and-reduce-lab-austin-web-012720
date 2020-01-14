89910d9686a098874cbbfab490a402535372d24e
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-14 03:06:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,10 +11,18 @@ def map(source_array)
      index += 1
     end
   new
-  
 end
     
 def reduce(source_array, starting_point = nil)
   new = []
-
+ index = 0
+    while index < source_array.length do
+  
+     yield(source_array[index])
+  
+     new.push(yield(source_array[index]))
+     index += 1
+    end
+  new
+end
 
