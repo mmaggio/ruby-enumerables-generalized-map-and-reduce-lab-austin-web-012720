c61d24b23fd6fce055a9af4c33f772864a959fac
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-14 03:16:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # Your Code Here
+
+def map(source_array)
+  new = []
+  index = 0
+    while index < source_array.length do
+  
+     yield(source_array[index])
+  
+     new.push(yield(source_array[index]))
+     index += 1
+    end
+  new
+end
+    
+def reduce(source_array, starting_value = nil)
+    if starting_value
+      index = 0
+      sum = starting_value
+    else
+      index = 1
+      sum = source_array[0]
+   end
+ 
+   while index < source_array.length do
+     sum = yield(sum, source_array[index])
+     index += 1
+   end
+  sum
+end
+
