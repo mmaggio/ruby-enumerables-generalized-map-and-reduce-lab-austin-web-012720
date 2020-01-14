365949adab4b1eff2889de0896f58fb5264890c9
Francis Michael Maggio <bluebassist2002@yahoo.com>
2020-01-14 02:22:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,6 @@
 # Your Code Here
 
-def map_to_negativize(source_array)
+def generalized_map(source_array)
   index = 0
   new_array = []
     while index < source_array.length do
@@ -11,7 +11,7 @@ def map_to_negativize(source_array)
   new_array
 end
 
-def map_to_no_change(source_array)
+def generalized_reduce (source_array, starting_value = 0)
   index = 0
   new_array = []
     while index < source_array.length do
@@ -21,63 +21,3 @@ def map_to_no_change(source_array)
   new_array
 end
 
-def map_to_double(source_array)
-  index = 0
-  new_array = []
-    while index < source_array.length do
-      double = source_array[index] * 2
-      new_array << double
-      index += 1
-    end
-  new_array
-end
-
-def map_to_square(source_array)
-  index = 0
-  new_array = []
-    while index < source_array.length do
-      square = source_array[index] ** 2
-      new_array << square
-      index += 1
-    end
-  new_array
-end
-
-
-
-def reduce_to_total(source_array, starting_point = 0)
-  partial_result = 0
-  index = 0
-  while index < source_array.length do
-      partial_result += source_array[index]
-      index += 1
-    end
-    result = partial_result + starting_point
-  result
-end
-
-def reduce_to_all_true(source_array)
-  index = 0
-  while index < source_array.length do
-      if !source_array[index]
-        answer = !!nil
-      else
-        answer = !!5
-      end
-      index += 1
-  end
-  return answer
-end
-
-def reduce_to_any_true(source_array)
- index = 0
-  while index < source_array.length do
-      if source_array[index]
-        answer = !!5
-      else
-        answer = !!nil
-      end
-      index += 1
-  end
-  return answer
-end
